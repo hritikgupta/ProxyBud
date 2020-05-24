a333eb784ee79dcbb25096bcc99346c6fab21f83
hritikgupta <hritikgupta9@gmail.com>
2020-05-24 13:08:30 +0530

    Added input validation
    Signed-off-by: hritikgupta <hritikgupta9@gmail.com>

--- index.sh
+++ index.sh
@@ -11,6 +11,36 @@ finalGreet(){
 	whiptail --title "** SUCCESS **" --msgbox "Thanks for using PROXYBUD.\n\nStar it, if you liked it : https://github.com/hritikgupta/ProxyBud" 10 78
 }
 
+function valid_ip(){
+    local  ip=$1
+    local  stat=1
+
+    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        OIFS=$IFS
+        IFS='.'
+        ip=($ip)
+        IFS=$OIFS
+        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
+            && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+        stat=$?
+    fi
+
+	if [ "$stat" -ne 0 ]; then
+		whiptail --textbox /dev/stdin 12 60 <<<"You have entered an invalid IP. Try again!\n\nExiting ..."
+		status=1
+		exit
+	fi
+}
+
+function valid_port(){
+	if [ -z "$1" ]
+	then
+		whiptail --textbox /dev/stdin 12 60 <<<"Port number not entered. Try again!\n\nExiting ..."
+		status=1
+		exit
+	fi
+}
+
 whiptail --title "** PROXYBUD **" --yesno "Hello, $USER\nLet's change your system proxy. Modifications will be made to :
 
     bashrc    apt.conf    environment-variable    network proxy    git
@@ -31,7 +61,11 @@ if [ $exitstatus = 0 ]; then
         case $CHOICE in
         		"1)") 
                 	prox=$(whiptail --inputbox "Enter Proxy Host : " 8 78 --title "Input" 3>&1 1>&2 2>&3)
+					valid_ip $prox
+
                 	port=$(whiptail --inputbox "Enter Proxy Port : " 8 78 --title "Input" 3>&1 1>&2 2>&3)
+					valid_port $port
+
                 	whiptail --title "Prompt" --yesno "Use the same proxy for https/ftp?" 8 78 
 					exitstatus=$?
 					if [ $exitstatus = 0 ]; then
@@ -43,9 +77,17 @@ if [ $exitstatus = 0 ]; then
 						exec sudo -S -p '' "bash" "set.sh" <<< "$pass" "$prox" "$port" "$prox" "$port" "$prox" "$port" 
 					else
 					    shost=$(whiptail --inputbox "Enter Proxy Host for HTTPS: " 8 78 --title "Input" 3>&1 1>&2 2>&3)
+						valid_ip $shost
+
                 		sport=$(whiptail --inputbox "Enter Proxy Port for HTTPS: " 8 78 --title "Input" 3>&1 1>&2 2>&3)
+						valid_port $sport
+
                 		fhost=$(whiptail --inputbox "Enter Proxy Host for FTP: " 8 78 --title "Input" 3>&1 1>&2 2>&3)
+						valid_ip $fhost
+
                 		fport=$(whiptail --inputbox "Enter Proxy Port for FTP: " 8 78 --title "Input" 3>&1 1>&2 2>&3)
+						valid_port $fport
+
                 		if [ "$(id -nu)" != "root" ]; then
 						    sudo -k
 						    pass=$(whiptail --title " Sudo access required " --passwordbox "Please authenticate to begin: \n\n[sudo] Password for user $USER:" 12 50 3>&2 2>&1 1>&3-)
